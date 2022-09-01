bcf5a0bb3b34f05bab3ef1dc422e9c4f3f408db9
Dino <dinopranjic92@gmail.com>
2022-09-01 15:47:11 -0400

    Adjust center of map on load

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,11 +2,11 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 import tileLayer from '../util/tileLayer';
 import 'leaflet/dist/leaflet.css';
 
-
+//51.505, -0.09
 
 function Map () {
   return(
-    <MapContainer center={[51.505, -0.09]} zoom={1} scrollWheelZoom={false} zoomControl={false}>
+    <MapContainer center={[5.1461, 34.1595]} zoom={1} scrollWheelZoom={false} zoomControl={false}>
       <TileLayer {...tileLayer} />
 
     </MapContainer>
