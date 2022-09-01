e600fed3ae348b1f34e9908a63f198ea9bd71960
Dino <dinopranjic92@gmail.com>
2022-09-01 15:25:02 -0400

    Remove zoom control from map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -6,7 +6,7 @@ import 'leaflet/dist/leaflet.css';
 
 function Map () {
   return(
-    <MapContainer center={[51.505, -0.09]} zoom={1} scrollWheelZoom={false}>
+    <MapContainer center={[51.505, -0.09]} zoom={1} scrollWheelZoom={false} zoomControl={false}>
       <TileLayer {...tileLayer} />
 
     </MapContainer>
