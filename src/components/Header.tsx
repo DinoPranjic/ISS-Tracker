50938a0b771c8d0991e9ae3c8b621bb734c51940
Dino <dinopranjic92@gmail.com>
2022-09-02 12:52:43 -0400

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header () {
+  return(
+    <HeaderContainer>
+      <HeaderLeft>
+        ISS Tracker
+      </HeaderLeft>
+
+      <HeaderRight>
+        Github Logo here.
+      </HeaderRight>
+    </HeaderContainer>
+  )
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  color: white;
+  background-color: #202225;
+
+`
+
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+`
+
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  /* align-items: flex-end; */
+  justify-content: flex-end;
+  margin-right: 10px;
+`
