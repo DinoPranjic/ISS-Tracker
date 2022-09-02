3b39cf5321446affb3c0bf4006b4cc0a75633041
Dino <dinopranjic92@gmail.com>
2022-09-02 12:53:21 -0400

    Add background and minor styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map';
+import Header from './components/Header';
+import styled from 'styled-components';
 
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
+    <AppBody>
 
-    <Map />
+      <Map />
+    </AppBody>
 
     </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+`
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,16 +1,26 @@
 import { MapContainer, TileLayer } from 'react-leaflet';
 import tileLayer from '../util/tileLayer';
 import 'leaflet/dist/leaflet.css';
+import styled from 'styled-components';
 
 //51.505, -0.09
 
 function Map () {
   return(
-    <MapContainer center={[5.1461, 34.1595]} zoom={1} scrollWheelZoom={false} zoomControl={false} dragging={false}>
-      <TileLayer {...tileLayer} />
+    <MapView>
+      <MapContainer center={[5.1461, 34.1595]} zoom={1} scrollWheelZoom={false} zoomControl={false} >
+        <TileLayer {...tileLayer} />
 
-    </MapContainer>
+      </MapContainer>
+    </MapView>
   )
 }
 
 export default Map;
+
+const MapView = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+`
