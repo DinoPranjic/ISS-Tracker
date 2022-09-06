bd9386490a2534a3a3913ff14034623def630e04
Dino <dinopranjic92@gmail.com>
2022-09-06 19:37:49 -0400

    Import Map provider into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,16 @@
-import React, {useState, useEffect} from 'react';
 import './App.css';
-import Map from './components/Map';
 import Header from './components/Header';
-import styled from 'styled-components';
+import { MapProvider } from './context/mapContext';
+import Container from './components/Container';
 
-
-function App() {
-//   const [latitude, setLatitude] = useState(0)
-//   const [longitude, setLongitude] = useState(0)
-
-//   const url: string = 'http://api.open-notify.org/iss-now.json'
-
-//   const getLocation = (url: string) => {
-//     return fetch(url)
-//   }
-
-// const updateLocation = () => {
-//     getLocation(url).then(async result => {
-//       const location = await result.json();
-
-//       const parsedLatitude = await parseFloat(location.iss_position.latitude)
-//       const parsedLongitude = await parseFloat(location.iss_position.longitude)
-      
-//       setLatitude(parsedLatitude);
-//       setLongitude(parsedLongitude);
-
-
-//     });
-//   }
-
-//   setTimeout(updateLocation, 5000)
-
+const App = () => {
   return (
-    <div className="App">
+    <MapProvider>
       <Header />
-    <AppBody>
-
-      <Map />
-    </AppBody>
-
-    </div>
+      <Container />
+    </MapProvider>
   );
 }
 
 export default App;
 
-const AppBody = styled.div`
-  display: flex;
-  height: 100vh;
-  justify-content: center;
-`
