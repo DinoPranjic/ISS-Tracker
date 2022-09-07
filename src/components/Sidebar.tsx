63dfe90d8f0ae4ce63f292f330d51b9545c2f690
Dino <dinopranjic92@gmail.com>
2022-09-07 14:25:20 -0400

    Render Layer picker component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import LayerPicker from "./LayerPicker";
 
 const SidebarContainer = styled.div`
   background-color: #202225;
@@ -7,7 +8,21 @@ const SidebarContainer = styled.div`
   max-width: 1000px;
   opacity: 0.8;
   margin-top: 5vh;
+  margin-bottom: 5vh;
+  margin-left: 2.5vw;
+  border: black 2px solid;
+  border-radius: 2%;
+`
+const SidebarHeader = styled.div`
   display: flex;
+  margin-top: 1vh;
+  justify-content: space-between;
+  margin-left: 0.5vw;
+  margin-right: 0.5vw;
+
+  i {
+    cursor: pointer;
+  }
 `
 
 
@@ -15,7 +30,14 @@ const SidebarContainer = styled.div`
 const Sidebar = () => {
   return (
     <SidebarContainer>
-
+      <SidebarHeader>
+        <strong>ISS Tracker</strong>
+        <i className="fa-brands fa-github"></i>
+      </SidebarHeader>
+      <hr />
+      Map Controls:
+      <LayerPicker />
+      <hr />
     </SidebarContainer>
   )
 }
