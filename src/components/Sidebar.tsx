6779b0d3e2371eb467f8f2e7a3d23beb2338ec25
Dino <dinopranjic92@gmail.com>
2022-09-06 21:21:21 -0400

    Style sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+
+const SidebarContainer = styled.div`
+  background-color: #202225;
+  color: white;
+  flex: 0.3;
+  max-width: 1000px;
+  opacity: 0.8;
+  margin-top: 5vh;
+  display: flex;
+`
+
+
+
 const Sidebar = () => {
-  
+  return (
+    <SidebarContainer>
+
+    </SidebarContainer>
+  )
 }
 
 export default Sidebar;
