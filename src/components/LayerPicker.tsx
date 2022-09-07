cfdeb5b161bb3164e161ff14bf575cd23c96f66b
Dino <dinopranjic92@gmail.com>
2022-09-07 14:22:44 -0400

    Create map layer picker component

---
+++ src/components/LayerPicker.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { useMapContext } from "../context/mapContext";
+import { streetLayer, hybridLayer, satelliteLayer, terrainLayer } from '../util/constants';
+import { ITileLayer } from "../context/types";
+
+const LayerPickerContainer = styled.div`
+  margin-left: 0.5vw;
+
+  span {
+    cursor: pointer;
+
+    :hover {
+      opacity: 0.8;
+    }
+  }
+`
+
+const LayerPicker = () => {
+  const { mapActions } = useMapContext();
+
+  const changeView = (layer: ITileLayer) => {
+      mapActions.setTileLayer({
+      url: layer.url,
+      maxZoom: layer.maxZoom as number,
+      subdomains: layer.subdomains,
+      key: layer.key
+    }) 
+  }
+
+  return (
+    <LayerPickerContainer>
+      View: <span onClick={() => {changeView(streetLayer)}}>Street</span> | <span onClick={() => {changeView(hybridLayer)}}>Hybrid</span> | <span onClick={() => {changeView(satelliteLayer)}}>Satellite</span> | <span onClick={() => {changeView(terrainLayer)}}>Terrain</span>
+    </LayerPickerContainer>
+  )
+}
+
+export default LayerPicker;
