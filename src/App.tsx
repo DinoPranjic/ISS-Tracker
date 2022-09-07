97199a9185433580c009fae3f9fac5026244ca18
Dino <dinopranjic92@gmail.com>
2022-09-06 21:21:03 -0400

    Add sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import './App.css';
+import styled from 'styled-components';
 import Header from './components/Header';
 import { MapProvider } from './context/mapContext';
 import Container from './components/Container';
+import Sidebar from './components/Sidebar';
 
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+
+`
 const App = () => {
   return (
     <MapProvider>
       <Header />
-      <Container />
+      <AppBody>
+        <Sidebar />
+        <Container />
+      </AppBody>
     </MapProvider>
   );
 }
